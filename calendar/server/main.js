6b5ce5efd9a94fb583cb5c5383a4b78fdaa08b95
Justin Riggio <nicemindz@gmail.com>
2017-01-30 09:32:03 -0500

    add facebook login

--- calendar/server/main.js
+++ calendar/server/main.js
@@ -1,5 +1,13 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  // first, remove configuration entry in case service is already configured
+	Accounts.loginServiceConfiguration.remove({
+	  service: "facebook"
+	});
+	Accounts.loginServiceConfiguration.insert({
+	  service: "facebook",
+	  appId: "659743730877343",
+	  secret: "4fdc7cfd6068285bf479ab8fc68a274a"
+	});
 });
